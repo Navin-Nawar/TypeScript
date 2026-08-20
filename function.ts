1671bb62f626bc33cab56d2b931d5748b0f32472
Navin-Nawar <navinvau17@gmail.com>
2026-08-20 12:03:16 +0600

    add new

---
+++ function.ts
@@ -0,0 +1,6 @@
+function add(num1:number,num2:number):number{
+    const total=num1+num2;
+    return total;
+}
+
+const result :number = add(3,5)
---
+++ function2.ts
@@ -0,0 +1,9 @@
+// function calculate():void{
+
+// }  -> return type na thakle void use korbo
+
+function calculate(arrrayofname:string[],tax:number,accountant?:string):boolean{
+    return false;
+}
+
+calculate(["nava","rava"],0.2)
