a4f4bced5daf9408a95230073426b272d321324a
Navin-Nawar <navinvau17@gmail.com>
2026-08-20 11:36:04 +0600

    new

---
+++ object.js
@@ -0,0 +1,8 @@
+"use strict";
+const student = {
+    name: "Navin",
+    age: 20,
+    ispassed: true
+};
+// student.age="navin"  ->  evabe use korte parbona
+console.log(student);
---
+++ object.ts
@@ -0,0 +1,8 @@
+const student:{name:string,age:20,ispassed:boolean} = {
+    name:"Navin",
+    age:20,
+    ispassed:true
+}
+// student.age="navin"  ->  evabe use korte parbona
+
+console.log(student)
